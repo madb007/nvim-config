b2ac3dc6b0b077ecb31a8e88e0a722f442c1a785
madb007 <bhuvanmadala@gmail.com>
2024-09-26 21:52:45 -0500

    Removed text highlighting conflict between treesitter and latex

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -18,9 +18,8 @@ vim.opt.rtp:prepend(lazypath)
 vim.opt.number = true
 
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = "unnamedplus"
 end)
 
 require("vim-options")
 require("lazy").setup("plugins")
-
